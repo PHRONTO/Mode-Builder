34ea54897c985ea5f2a6f4573b28721f04e54158
PHRONTO <callnightmaresadk@gmail.com>
2023-05-19 13:15:56 +0400

    File

---
+++ __init__.py
@@ -0,0 +1,9 @@
+bl_info = {
+    "name": "Model-Builder",
+    "author": "PHRONTO",
+    "version": (1, 0),
+    "blender": (3, 4, 0),
+    "location": "",
+    "description": "Universal Addon",
+    "category": "3D View"
+}
